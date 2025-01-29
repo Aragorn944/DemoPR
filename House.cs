a864a02dc105a6a73a6db6d3e928d02817cebecc
Aragorn944 <stoyanmarinov007@gmail.com>
2025-01-29 21:02:01 +0200

    Create House.cs
    PR

---
+++ House.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class House
+{
+    // Properties of the house
+    public string Address { get; set; }
+    public int NumberOfRooms { get; set; }
+    public double Price { get; set; }
+
+    // Constructor to initialize the house object
+    public House(string address, int numberOfRooms, double price)
+    {
+        Address = address;
+        NumberOfRooms = numberOfRooms;
+        Price = price;
+    }
+
+    // Method to display house details
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Address: {Address}");
+        Console.WriteLine($"Number of Rooms: {NumberOfRooms}");
+        Console.WriteLine($"Price: ${Price}");
+    }
+
+    // Main method to test the class
+    public static void Main(string[] args)
+    {
+        // Create an instance of House
+        House myHouse = new House("1234 Elm St", 3, 250000);
+
+        // Display details of the house
+        myHouse.DisplayDetails();
+    }
+}
